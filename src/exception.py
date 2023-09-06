7d9cc001e9474692b5c147b145ab0be390b8598f
Chayan Rellan <rellanchayan@gmail.com>
2023-09-06 14:51:11 -0400

    Removed the testing from exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -16,18 +16,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
-
-
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
-
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
-logging.basicConfig(
-    filename = LOG_FILE_PATH,
-    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level = logging.INFO,
-
-)
-
